1662abcdc15b74f207b4cdcd34e0a080d9a415f5
Mykhailo Hrynko <mykhailo.hrynko@solidgate.com>
2026-01-24 17:59:32 +0200

    remove sh files

--- 1.load.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd grinder
-fnm use 2>/dev/null
-# npm run load > logs/load.log
-echo "Load script is commented out. Press Enter to continue..."
-read
--- 2.summarize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd grinder
-fnm use 24 2>/dev/null
-npm run summarize > logs/summarize.log
-echo "Summarize complete. Press Enter to continue..."
-read
